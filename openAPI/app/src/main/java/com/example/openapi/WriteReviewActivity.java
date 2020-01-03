b7e500b23e84ea3ec9b1aadb29ee7813bd392b85
koo5590 <40734412+koo5590@users.noreply.github.com>
2020-01-04 00:34:26 +0900

    Update WriteReviewActivity.java

--- openAPI/app/src/main/java/com/example/openapi/WriteReviewActivity.java
+++ openAPI/app/src/main/java/com/example/openapi/WriteReviewActivity.java
@@ -9,7 +9,9 @@ import android.widget.EditText;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -30,6 +32,10 @@ public class WriteReviewActivity extends AppCompatActivity {
     Button saveButton;      //save reviews
     EditText newPost;       //contains user input
     WeddingObj weddingObj;  //object of this wedding place
+    String state;
+    ReviewItem item;
+    String from;
+    String weddingName;
 
     //Firebase realtime DB and Authentication
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -42,10 +48,17 @@ public class WriteReviewActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         weddingObj = (WeddingObj)intent.getSerializableExtra("object");
+        state = (String)intent.getSerializableExtra("state");
+        weddingName = (String)intent.getSerializableExtra("name");
 
         mAuth = FirebaseAuth.getInstance();
         newPost = (EditText)findViewById(R.id.writeText);
         saveButton = (Button)findViewById(R.id.saveB);
+
+        if(state.equals("edit")){
+            item = (ReviewItem)intent.getSerializableExtra("item");
+            newPost.append(item.getContent());
+        }
 
         //go bakc to detail page without writing review
         findViewById(R.id.back).setOnClickListener(new View.OnClickListener(){
@@ -79,18 +92,30 @@ public class WriteReviewActivity extends AppCompatActivity {
                                     Toast.makeText(WriteReviewActivity.this, "후기는 50자 이상을 남겨주세요!", Toast.LENGTH_SHORT).show();
                                     return;
                                 }
-
                                 //get user info
                                 String uid = mAuth.getCurrentUser().getUid();
-                                String key= myRef.child("posts").push().getKey();
 
-                                //create new ReviewItem object and save data in DB
-                                ReviewItem newReview = new ReviewItem(weddingObj.getName(),content, nickname, date);
-                                myRef.child("posts").child(weddingObj.getName()).child(key).setValue(newReview);
-                                myRef.child("users").child(uid).child("posts").child(key).setValue(newReview);
+                                //new post
+                                if(state.equals("new")) {
+                                    String key = myRef.child("posts").push().getKey();
 
-                                //alert user
-                                Toast.makeText(WriteReviewActivity.this, "소중한 후기를 남겨주셔서 감사합니다 :)", Toast.LENGTH_SHORT).show();
+                                    //create new ReviewItem object and save data in DB
+                                    ReviewItem newReview = new ReviewItem(weddingObj.getName(), content, nickname, date, key);
+                                    myRef.child("posts").child(weddingObj.getName()).child(key).setValue(newReview);
+                                    myRef.child("users").child(uid).child("posts").child(key).setValue(newReview);
+
+                                    //alert user
+                                    Toast.makeText(WriteReviewActivity.this, "소중한 후기를 남겨주셔서 감사합니다 :)", Toast.LENGTH_SHORT).show();
+                                }
+
+                                else{
+                                    Map<String, Object> taskMap = new HashMap<>();
+                                    taskMap.put(item.getId()+"/content", content);
+                                    taskMap.put(item.getId()+"/date",new SimpleDateFormat("yyyy-MM-EE", Locale.getDefault()).format(currentTime));
+                                    myRef.child("posts").child(weddingName).updateChildren(taskMap);
+                                    myRef.child("users").child(uid).child("posts").updateChildren(taskMap);
+                                    Toast.makeText(WriteReviewActivity.this, "글이 수정되었습니다 :)", Toast.LENGTH_SHORT).show();
+                                }
 
                                 //go back to detail page
                                 Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
